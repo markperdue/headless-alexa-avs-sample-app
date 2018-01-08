7e4ef432efec1d95256153f3a9cd90427f4f3604
Mark Perdue <markaperdue@gmail.com>
2018-01-07 21:02:22 -0500

    Update wakeword.sh

--- scripts/wakeword.sh
+++ scripts/wakeword.sh
@@ -2,4 +2,4 @@
 cd ~/alexa-avs-sample-app/samples
 sleep 45
 aplay /home/pi/headless-alexa-avs-sample-app/Startup.wav
-cd wakeWordAgent/src && sudo ./wakeWordAgent -e kitt_ai
+cd wakeWordAgent/src && ./wakeWordAgent -e kitt_ai
